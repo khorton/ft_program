db5f306dd04eca70741bb8bf4df47f9d3de48a05
Kevin Horton <kevin01@kilohotel.com>
2015-08-03 06:52:02 -0400

    Change name for backup file to ft_program_multi

--- bu_ft_program.sh
+++ bu_ft_program.sh
@@ -8,6 +8,7 @@ if [ -f /home/kwh/hg/ft_program/test_card.pl ]; then
   /usr/bin/mysqldump -u ft -pft --order-by-primary --skip-extended-insert ft_program > ft_program_bu.sql
 else
   echo "Not on EeePC"
-  /sw/bin/mysqldump -u ft -pft --order-by-primary --skip-extended-insert ft_program > ft_program_bu.sql
+  /sw/bin/mysqldump -u ft -pft --order-by-primary --skip-extended-insert ft_program_multi > ft_program_multiag_bu.sql
+  # /sw/bin/mysqldump -h 127.0.0.1 -u ft -pft --order-by-primary --skip-extended-insert ft_program_multi > ft_program_mulit_bu.sql
 fi
 
